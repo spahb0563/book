10d4c58efaedfad50598a0163d3e76d13f20e0b4
Moon <spahb0563@naver.com>
2022-09-07 11:15:35 +0900

    배포된 사이트 사용자 이름 출력 문제 해결

--- src/main/java/com/example/book/web/IndexController.java
+++ src/main/java/com/example/book/web/IndexController.java
@@ -5,13 +5,10 @@ import com.example.book.config.auth.LoginUser;
 import com.example.book.config.auth.dto.SessionUser;
 import com.example.book.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
@@ -23,7 +20,7 @@ public class IndexController {
         model.addAttribute("posts", postsService.findAllDesc());
 
         if(user != null) {
-            model.addAttribute("user", user.getName());
+            model.addAttribute("un", user.getName());
         }
         return "index";
     }
